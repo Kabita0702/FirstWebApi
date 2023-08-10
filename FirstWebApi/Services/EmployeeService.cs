dd7bd29cfff2c4d09434514d06fa0036a4fdf3ba
Kabita <Kabita@DESKTOP-2GKRI19>
2023-08-10 09:06:38 -0700

    Add project files.

---
+++ FirstWebApi/Services/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using FirstWebApi.EmployeeData;
+using FirstWebApi.Json;
+using FirstWebApi.Models;
+
+namespace FirstWebApi.Services
+{
+    public class EmployeeService
+    {
+        public Employee GetEmployee(int id)
+        {
+            var employeeList = StoreData.GetEmployeeData();
+            var employee = employeeList.FirstOrDefault(x => x.Id == id);
+            if (employee == null)
+            {
+                throw new Exception("Data not found");
+            }
+            return employee;
+        }
+    }
+}
